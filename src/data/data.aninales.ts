03bf8f266408c1d69286d0e5e0ed04cb5d5a58b7
santi <ojedabotansantiago@gmail.com>
2018-05-05 02:16:19 +0200

    sonidos 2

---
+++ src/data/data.aninales.ts
@@ -0,0 +1,20 @@
+
+export const ANIMALES = [{
+    nombre: "caballo",
+    imagen: "assets/animales/caballo.png",
+    audio: "assets/sonidos/caballo.mp3",
+    duracion: 4,
+    reproduciendo: false
+},{
+    nombre: "cabra",
+    imagen: "assets/animales/cabra.png",
+    audio: "assets/sonidos/cabra.wav",
+    duracion: 4,
+    reproduciendo: false
+},{
+    nombre: "cerdo",
+    imagen: "assets/animales/cerdo.png",
+    audio: "assets/sonidos/cerdo.wav",
+    duracion: 4,
+    reproduciendo: false
+}];
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { ANIMALES } from "../../data/data.aninales";
+import { Animal } from './../../interfaces/animal.interface';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  animales:any[] = [];
+  constructor() {
+    this.animales = ANIMALES.splice(0);
+  }
+  reproducir(animal:Animal){
+    console.log(animal);
+    let audio = new Audio;
+    audio.src = animal.audio;
 
-  constructor(public navCtrl: NavController) {
+    audio.load();
+    audio.play();
 
+    animal.reproduciendo = true;
+    
+    setTimeout(() => {
+      animal.reproduciendo = false,
+      animal.duracion * 1000
+    },1000);
+  }
+
+  isStop(animal:Animal) {
+    return animal.reproduciendo;
+  }
+
+  isPlay(animal:Animal){
+    return !animal.reproduciendo;
   }
 
 }
